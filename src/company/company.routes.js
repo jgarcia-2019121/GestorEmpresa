816eabbb6d914397c0dd6cad380201b3ec224eee
jgarcia-2019121 <jgarcia-2019121@kinal.edu.gt>
2024-03-03 21:52:30 -0600

    Subiendo los request del Gestor de Empresa

--- src/company/company.routes.js
+++ src/company/company.routes.js
@@ -4,7 +4,7 @@ import { Router } from 'express'
 import {
     create,
     update,
-    getCompany,
+    get,
     getExperiences,
     getAZ,
     getZA,
@@ -15,8 +15,7 @@ const api = Router()
 
 api.post('/create', create);
 api.put('/update:id', update);
-api.get('/getCompany', getCompany);
-api.get('/getExperiences', getExperiences);
+api.get('/get', get);
 api.get('/getExperiences', getExperiences);
 api.get('/getAZ', getAZ);
 api.get('/getZA', getZA);
